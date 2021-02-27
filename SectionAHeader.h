a57941edbe6e5977e4d960a40f70e6757510917b
Halay395 <66864762+Halay395@users.noreply.github.com>
2021-02-26 22:19:52 -0800

    added some changes to SectionAHeader.h

--- SectionAHeader.h
+++ SectionAHeader.h
@@ -1,44 +1,21 @@
-#include <iostream>
+#pragma once
 #include <string>
 
-
 class URL {
+
+
+public:
+	URL(std::string urlString);
+
+	std::string getURL();
+	std::string getScheme();
+	std::string getAuthority();
+	std::string getPath();
+
 private:
 
 
-    std::string scheme;
-    std::string authority;
-    std::string path;
+	std::string myURL;
 
-public:
-    URL(std::string url) {
-        int first = url.find(":");
-        scheme = url.substr(0, first);
-        std::string restUrl = url.substr(first + 3, url.length());
-        int thirdIndex = restUrl.find("/");
-        authority = url.substr(first + 3, thirdIndex);
-        path = restUrl.substr(thirdIndex, url.length());
 
-    }
-    std::string getAuthority() {
-        return authority;
-    }
-    std::string getScheme() {
-        return scheme;
-    }
-   
-    std::string getPath() {
-        return path;
-    }
-    std::string getFullURL() {
-        return scheme + "://" + authority + path;
-    }
 };
-
-
-
-
-
-
-
-
